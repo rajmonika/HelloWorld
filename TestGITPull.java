1ae88c27bd17a99acb4ae6292313271f6cd58b24
rajmonika <43081528+rajmonika@users.noreply.github.com>
2020-09-28 20:41:38 +0530

    Update TestGITPull.java

--- TestGITPull.java
+++ TestGITPull.java
@@ -1,1 +1,42 @@
-Test the git pull
+public class OrderedBST {
+	
+	// Java program to check if a given array is sorted 
+	// or not. 
+
+	
+
+	// Function that returns true if array is Inorder 
+	// traversal of any Binary Search Tree or not. 
+		static boolean isInorder(int[] arr, int n) { 
+			// Array has one or no element 
+			if (n == 0 || n == 1) { 
+				return true; 
+			} 
+
+			for (int i = 1; i < n; i++) // Unsorted pair found 
+			{ 
+				if (arr[i - 1] > arr[i]) { 
+					return false; 
+				} 
+			} 
+
+			// No unsorted pair found 
+			return true; 
+		} 
+	// Drivers code 
+
+		public static void main(String[] args) { 
+			int arr[] = {1,2,2,4,7,2}; 
+			int n = arr.length; 
+			if (isInorder(arr, n)) { 
+				System.out.println("Yes"); 
+			} else { 
+				System.out.println("No"); 
+			} 
+		} 
+	} 
+
+
+
+
+
